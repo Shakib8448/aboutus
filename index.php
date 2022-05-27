d2d6433209b41348238324eeaa0a67aca893ce50
Shakib8448 <85308153+Shakib8448@users.noreply.github.com>
2022-05-27 20:13:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Preview About</title>
+</head>
+<body>
+<div class="wrapper">
+
+    <div class="background-container">
+        <div class="bg-1"></div>
+        <div class="bg-2"></div>
+
+    </div>
+    <div class="about-container">
+
+        <div class="image-container">
+            <img src="aboutus.jpg">
+
+        </div>
+
+        <div class="text-container">
+            <h1>About us</h1>
+            <p>Loremit. Eligendi porro rerum cupiditate expedita <br> distinctio nisi nulla ipsum libero molestiae minima Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,ipsum libero molestiae</p>
+            <a href="">Read More</a>
+        </div>
+
+    </div>
+</div>
+
+
+</body>
+</html>
